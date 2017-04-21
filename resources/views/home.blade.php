fc81ab8e47a085c6c1cc61ac10cf667f7813c3c8
Sean Misra <ssm937@g.harvard.edu>
2017-04-21 06:51:29 -0400

    Homepage styled: Bootstrap+images

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,26 @@
 @extends('layouts.master')
 
+@push('head')
+    <style> 
+        body {
+            text-align: center; 
+        }
+        h1 {
+            font-size: 70px; 
+        }
+        input {
+            width: 50vw; 
+            height: 65px;
+            font-size: 25px; 
+        }
+        button {
+            width: 15vw;
+            height: 65px; 
+            font-size: 25px; 
+        } 
+    </style>
+@endpush 
+
 @section('title')
     Dog Database
 @stop
@@ -10,7 +31,7 @@
     </header>
 
     <main>
-        <h2>Search/Match</h2> 
+        <h2>Search | Match</h2> 
         
         @if(count($errors) > 0)
             <ul>
@@ -22,7 +43,9 @@
         @endif
         
         <form method='GET' action='{{ action("HomeController@search") }}'>
-            <input type='text' name='search' placeholder='Type breed...' required>
+            <div class = 'form-group'>
+                <input type='text' name='search' placeholder='Type breed...' required>
+            </div>
             <br> 
             <button type='submit'>Submit</button>
         </form>  
@@ -32,3 +55,18 @@
         <p>©2017</p>
     </footer>
 @stop
+
+@push('body')
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+    integrity="sha256-k2WSCIexGzOj3Euiig+TlR8gA0EmPjuc79OEeY5L45g="
+    crossorigin="anonymous"></script>
+    <script> 
+        var randArray = ["one", "two", "three", "four", "five", "six", "seven"]; 
+        var randNumb = Math.round(Math.random()*6); 
+        var imageNumb = randArray[randNumb];
+        path = "images/cover_"+imageNumb+".png";
+        console.log(randNumb);
+        
+        $('body').css({'background': 'url(' + path + ')' + 'no-repeat 50% 50% fixed', 'background-size': 'cover'}); 
+    </script>
+@endpush
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,13 +1,27 @@
 <!doctype html> 
-<html> 
-    <meta charset="utf-8">
-    <title>
-        @yield('title', 'Foobooks')
-    </title>
-<head>
-</head>
+<html lang='en'> 
+    <head>
+        {{-- Metadata --}}
+        <meta charset="utf-8">
+        <meta name = 'description' content='Find and search for dogs'>
+        <meta name ='author' content='Sean Misra'>
+        <meta name='keywords' content='Dogs, Pets, Harvard Extension, Laravel, PHP, DWA15, Sean Misra'>
+        <title>
+            @yield('title', 'Foobooks')
+        </title>
+        
+        {{-- Global CSS --}}
+        <link href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u' crossorigin='anonymous'>
+        
+        {{-- Any additional CSS dependencies should go here --}}
+        @stack('head')
+    </head>
 
-<body>
-    @yield('content')
-</body>
+    <body>
+        {{-- All body content should go here --}}
+        @yield('content')
+        
+        {{-- Any additional JS dependencies should go here --}}
+        @stack('body')
+    </body>
 </html>
