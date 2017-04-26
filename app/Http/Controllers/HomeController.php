0e2dfc23e5b79cfbe9a2f96547db0980c7fe9106
Sean Misra <ssm937@g.harvard.edu>
2017-04-26 19:08:07 -0400

    Cut duplicate Controller code

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,53 +23,8 @@ class HomeController extends Controller
             
             return redirect('/breeds')->withErrors($validator)->withInput(Input::all());   
         }
-        else {        
-            // create image path
-            $imagePath = "images/".str_replace(" ", "_", $dog).".jpg"; 
-            
-            // get group
-            $group = Dog::where('name', 'LIKE', $dog)->pluck('group');
-            $group = $group[0]; 
-            
-            //get 4 similar breeds (based for now only on Group)
-            $similarBreeds = Dog::where('name', '!=', $dog)->where('group', 'LIKE', $group)->limit(4)->pluck('name')->toArray();
-            $similarBreedImgs = []; 
-            foreach ($similarBreeds as $breed) {
-                $similarBreedImgs [] = "images/".str_replace(" ", "_", $breed).".jpg";
-            }
-            
-            // get energy
-            $energy = Dog::where('name', 'LIKE', $dog)->pluck('energy');
-            $energy = (int) $energy[0]; 
-
-            // get social
-            $social = Dog::where('name', 'LIKE', $dog)->pluck('social');
-            $social = (int) $social[0]; 
-
-            // get intelligence
-            $intelligence = Dog::where('name', 'LIKE', $dog)->pluck('intelligence');
-            $intelligence = (int) $intelligence[0]; 
-
-            // get cleanliness
-            $cleanliness = Dog::where('name', 'LIKE', $dog)->pluck('cleanliness');
-            $cleanliness = (int) $cleanliness[0]; 
-
-            // get adventure
-            $adventure = Dog::where('name', 'LIKE', $dog)->pluck('adventure');
-            $adventure = (int) $adventure[0]; 
-            
-            return view('dog')->with([
-                'dog' => $dog,  
-                'group' => $group,
-                'energy' => $energy, 
-                'social' => $social,
-                'intelligence' => $intelligence,
-                'cleanliness' => $cleanliness,
-                'adventure' => $adventure,
-                'imagePath' => $imagePath, 
-                'similarBreeds' => $similarBreeds,
-                'similarBreedImgs' => $similarBreedImgs
-            ]);
+        else {   
+            return redirect('/breeds/'.$dog);
         }
     }
     
