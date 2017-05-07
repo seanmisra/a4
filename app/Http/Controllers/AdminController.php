2a5518a3d2e51282d02127859a1e4596b609c551
Sean Misra <ssm937@g.harvard.edu>
2017-05-07 15:42:45 -0400

    Admin Panel Validation+Interface

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -10,10 +10,22 @@ use App\Dog;
 class AdminController extends Controller
 {
     public function main() {
-        return view('admin');
+        # get all dog names
+        $allDogs = Dog::all()->pluck('name')->toArray(); 
+        sort($allDogs); 
+        $allDogs = json_encode($allDogs); 
+        
+        return view('admin')->with([
+            'allDogs' => $allDogs
+        ]);
     }
     
     public function search(Request $request) {
+        # get all dog names
+        $dogNames = Dog::all()->pluck('name')->toArray(); 
+        sort($dogNames); 
+        $dogNames = json_encode($dogNames); 
+        
         $dog = ucwords($request->input('adminSearch')); 
         $actionType = $request->input('actionType'); 
         
@@ -34,11 +46,38 @@ class AdminController extends Controller
         
         return view('admin')->with([
             'dog' => $dogFull,
-            'actionType' => $actionType
+            'actionType' => $actionType,
+            'allDogs' => $dogNames
         ]); 
     }
     
     public function edit(Request $request) {
+        $rules = [
+            'name' => 'required|regex:/^[\pL\s\-.]+$/u',
+            'group' => 'required|in:Herding,Hound,Non-Sporting,Sporting,Working,Toy',
+            'apartment' => 'required|boolean',
+            'size' => 'required|in:tiny,small,medium,large',
+            'energy' => 'required|integer|between:1,5',
+            'social' => 'required|integer|between:1,5',
+            'intelligence' => 'required|integer|between:1,5',
+            'cleanliness' => 'required|integer|between:1,5',
+            'adventure' => 'required|integer|between:1,5'
+        ];
+        
+        if($request->has('aliasOne'))
+            $rules['aliasOne'] = 'regex:/^[\pL\s\-.]+$/u'; 
+        if($request->has('aliasTwo'))
+            $rules['aliasTwo'] = 'regex:/^[\pL\s\-.]+$/u'; 
+        if($request->has('aliasThree'))
+            $rules['aliasThree'] = 'regex:/^[\pL\s\-.]+$/u'; 
+
+        
+        $validator = Validator::make($request->all(), $rules); 
+
+        if ($validator->fails()) {
+            return redirect('/admin')->withErrors($validator)->withInput(Input::all());   
+        }
+        
         $dog = Dog::find($request->id); 
         
         $dog->name = $request->name; 
@@ -70,17 +109,17 @@ class AdminController extends Controller
     
     public function add(Request $request) {
         $dog = new Dog(); 
-                
+                        
         $rules = [
             'name' => 'required|regex:/^[\pL\s\-.]+$/u',
             'group' => 'required|in:Herding,Hound,Non-Sporting,Sporting,Working,Toy',
             'apartment' => 'required|boolean',
             'size' => 'required|in:tiny,small,medium,large',
-            'energy' => 'required|between:1,5',
-            'social' => 'required|between:1,5',
-            'intelligence' => 'required|between:1,5',
-            'cleanliness' => 'required|between:1,5',
-            'adventure' => 'required|between:1,5'
+            'energy' => 'required|integer|between:1,5',
+            'social' => 'required|integer|between:1,5',
+            'intelligence' => 'required|integer|between:1,5',
+            'cleanliness' => 'required|integer|between:1,5',
+            'adventure' => 'required|integer|between:1,5'
         ];
         
         if($request->has('aliasOne'))
@@ -116,9 +155,7 @@ class AdminController extends Controller
         
         
         $dog->save();   
-        
-        
-        
+    
                 
         dd("Added dog!"); 
     }
--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -6,6 +6,9 @@
 
 @section('content')
     <style>
+        .navbar {
+            background-color: beige; 
+        }
         span {
             font-weight: bold; 
         }
@@ -13,50 +16,67 @@
             opacity: .8; 
             cursor: pointer; 
         }
-        input, select, button {
+        input, select, button, .form-control {
             height: 100px; 
             width: 50vw; 
             font-size: 25px; 
         }
         main {
-            margin-left: 50px; 
+            margin-left: 25vw; 
         }
-
+        header {
+            text-align: center; 
+        }
+        #option, .adminSearch {
+            height: 125px !important; 
+            font-size: 35px !important; 
+            font-weight: bold; 
+        } 
+        p {
+            font-size: 20px; 
+        }
     </style>
     
-    <main>
-        <header>
+    
+    <header>
+        <div class='jumbotron'>
             <h1>Dog Database Admin</h1>
-        </header>
+            <p>~Bare-Bones: Add/Edit/Delete~</p>
+            <i class="fa fa-paw" aria-hidden="true" style="font-size: 50px;"></i>
+            <br>
+        </div>
+        
         @if (count($errors) > 0)
-            <div class="alert alert-danger">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
+            <div class="alert alert-danger" style="margin: 0 0 0 0;">
+                @foreach ($errors->all() as $error)
+                    <strong>{{ $error }}</strong>
+                <br>
+                @endforeach
             </div>
         @endif
-        
+        <br><br>
+    </header>
+
+    <main>
         <select class='form-control' id='option' name='option' style="height: 100px; font-size: 25px; width: 50vw;">
             <option value='Add'>Add</option>
             <option value='Edit' {{ isset($actionType) ? (($actionType == 'edit') ? 'SELECTED' : ''):'' }}>Edit</option>
             <option value='Delete' {{ isset($actionType) ? (($actionType == 'delete') ? 'SELECTED' : ''):'' ? 'SELECTED' : '' }}>Delete</option>
         </select>
-        <p>*Denotes a <strong>required</strong> field</p>
+        <p>*Denotes a <strong>required</strong> field</p><br><br>
         
         
         <form class= "add" method="POST" style='display:none;' action='{{ action("AdminController@add")}}'> 
             {{ csrf_field() }}
-            <h3>*Name</h3> 
+            <h3>*Name:</h3> 
             <input type='text' name='name' placeholder='Enter dog name (required)'>
-            <h3>AliasOne</h3>
+            <h3>AliasOne:</h3>
             <input type='text' name='aliasOne' placeholder='Enter Alias (Optional)'>
-            <h3>AliasTwo</h3>
+            <h3>AliasTwo:</h3>
             <input type='text' name='aliasTwo' placeholder='Enter Alias (Optional)'>
-            <h3>AliasThree</h3>
+            <h3>AliasThree:</h3>
             <input type='text' name='aliasThree' placeholder='Enter Alias (Optional)'>        
-            <h3>*Group</h3>
+            <h3>*Group:</h3>
             <select class='form-control' name='group'>                        
                 <option value='Herding'>Herding</option>
                 <option value='Hound'>Hound</option>
@@ -66,13 +86,13 @@
                 <option value='Toy'>Toy</option>
             </select>
 
-            <h3>*Apartment</h3>
+            <h3>*Apartment:</h3>
             <select class='form-control' name='apartment'>                        
                 <option value='0'>Not Apartment-Friendly</option>
                 <option value='1'> Apartment-Friendly</option>
             </select>                    
 
-            <h3>*Size</h3>
+            <h3>*Size:</h3>
             <select class='form-control' name='size'>
                 <option value='tiny'>Tiny</option>
                 <option value='small'>Small</option>
@@ -80,15 +100,15 @@
                 <option value='large'>Large</option>
             </select>                                        
 
-            <h3>*Energy</h3>
+            <h3>*Energy:</h3>
             <input type='text' name='energy' placeholder='Enter number 1-5'>
-            <h3>*Social</h3>
+            <h3>*Social:</h3>
             <input type='text' name='social' placeholder='Enter number 1-5'>
-            <h3>*Intelligence</h3>
+            <h3>*Intelligence:</h3>
             <input type='text' name='intelligence' placeholder='Enter number 1-5'>
-            <h3>*Cleanliness</h3>
+            <h3>*Cleanliness:</h3>
             <input type='text' name='cleanliness' placeholder='Enter number 1-5'>
-            <h3>*Fun</h3>
+            <h3>*Fun:</h3>
             <input type='text' name='adventure' placeholder='Enter number 1-5'>
             <br>
             <br>
@@ -101,16 +121,19 @@
         
         <form class ='edit' method="GET" action='{{ action("AdminController@search") }}'> 
             <input type='hidden' name='actionType' value='edit'>
-            <input type='text' placeholder='Search dog to edit' name='adminSearch' style="width:50vw; height: 100px; font-size: 25px;">  
+            <h3>Search: </h3>
+            <input type='text' placeholder='Search dog to edit' class='adminSearch' name='adminSearch' style="width:50vw; height: 100px; font-size: 25px; margin-bottom: 200px;">  
         </form>
         
         <form class ='delete' method="GET" action='{{ action("AdminController@search") }}'> 
             <input type='hidden' name='actionType' value='delete'>
-            <input type='text' placeholder='Search dog to delete' name='adminSearch' style="width:50vw; height: 100px; font-size: 25px;">  
+            <h3>Search: </h3>
+            <input type='text' placeholder='Search dog to delete' class='adminSearch' name='adminSearch' style="width:50vw; height: 100px; font-size: 25px; margin-bottom: 200px;">  
+            <br><br><br>
         </form>
         
         @if(isset($dog))
-            <div class='dogSection'>
+            <div class='dogSection' style="margin-top: -160px;">
                 <div class='edit'>       
                     <h2>Current Dog: {{ $dog->name }}</h2>
                 </div>  
@@ -121,17 +144,17 @@
                 <form class='edit' method='post' style='display:none;' action='{{ action("AdminController@edit") }}'>
                     {{ csrf_field() }}
                     <input type='hidden' name='id' value='{{ $dog->id }}'>
-                    <h3>*Name</h3> 
+                    <h3>*Name:</h3> 
                     <input type='text' name='name' value='{{ $dog->name }}'>
-                    <h3>AliasOne</h3>
-                    <input type='text' name='aliasOne' value='{{ $dog->aliasOne }}'>
-                    <h3>AliasTwo</h3>
-                    <input type='text' name='aliasTwo' value='{{ $dog->aliasTwo }}'>
-                    <h3>AliasThree</h3>
-                    <input type='text' name='aliasThree' value='{{ $dog->aliasThree }}'>
+                    <h3>AliasOne:</h3>
+                    <input type='text' name='aliasOne' placeholder='AliasOne' value='{{ $dog->aliasOne }}'>
+                    <h3>AliasTwo:</h3>
+                    <input type='text' name='aliasTwo' placeholder='AliasTwo' value='{{ $dog->aliasTwo }}'>
+                    <h3>AliasThree:</h3>
+                    <input type='text' name='aliasThree' placeholder='AliasThree' value='{{ $dog->aliasThree }}'>
                     
-                    <h3>*Group</h3>
-                    <select class='form-control' name='group'>                        
+                    <h3>*Group:</h3>
+                    <select class='form-control' name='group'>
                         <option value='Herding' {{ ($dog->group == 'Herding') ? 'SELECTED' : '' }}> Herding</option>
                         <option value='Hound' {{ ($dog->group == 'Hound') ? 'SELECTED' : '' }}>Hound</option>
                         <option value='Non-Sporting' {{ ($dog->group == 'Non-Sporting') ? 'SELECTED' : '' }}>Non-Sporting</option>
@@ -140,13 +163,13 @@
                         <option value='Toy' {{ ($dog->group == 'Toy') ? 'SELECTED' : '' }}>Toy</option>
                     </select>
                     
-                    <h3>*Apartment</h3>
+                    <h3>*Apartment:</h3>
                     <select class='form-control' name='apartment'>                        
                         <option value='0' {{ ($dog->apartment == 0) ? 'SELECTED' : '' }}>Not Apartment-Friendly</option>
                         <option value='1' {{ ($dog->apartment == 1) ? 'SELECTED' : '' }}> Apartment-Friendly</option>
                     </select>                    
                     
-                    <h3>*Size</h3>
+                    <h3>*Size:</h3>
                     <select class='form-control' name='size'>
                         <option value='tiny' {{ ($dog->size == 'tiny') ? 'SELECTED' : '' }}>Tiny</option>
                         <option value='small' {{ ($dog->size == 'small') ? 'SELECTED' : '' }}> Small</option>
@@ -155,15 +178,15 @@
                     </select>                                        
                     
                     
-                    <h3>*Energy</h3>
+                    <h3>*Energy:</h3>
                     <input type='text' name='energy' value='{{ $dog->energy }}'>
-                    <h3>*Social</h3>
+                    <h3>*Social:</h3>
                     <input type='text' name='social' value='{{ $dog->social }}'>
-                    <h3>*Intelligence</h3>
+                    <h3>*Intelligence:</h3>
                     <input type='text' name='intelligence' value='{{ $dog->intelligence }}'>
-                    <h3>*Cleanliness</h3>
+                    <h3>*Cleanliness:</h3>
                     <input type='text' name='cleanliness' value='{{ $dog->cleanliness }}'>
-                    <h3>*Fun</h3>
+                    <h3>*Fun:</h3>
                     <input type='text' name='adventure' value='{{ $dog->adventure }}'>
                     <br>
                     <br>
@@ -184,9 +207,28 @@
 @stop
 
 @push('body')
-    <script src="https://code.jquery.com/jquery-3.2.1.min.js"
-        integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
-        crossorigin="anonymous"></script>
+    <script>
+        var allDogs = {!! $allDogs !!}; 
+        
+        // create jQuery autocomplete validation 
+        // only matches beginning of word
+        // ignores white spaces
+        $(".adminSearch").autocomplete({
+            source: function(request, response) {
+                var re = ($.ui.autocomplete.escapeRegex((request.term).replace(" ", "")));
+                if (re == "") {
+                    return false; 
+                }
+
+                var matcher = new RegExp("^" + re, "i" );
+                var result = $.grep(allDogs, function(item,index){
+                    item = item.replace(" ", ""); 
+                    return matcher.test(item);
+                });
+                response(result);
+            }
+        }); 
+    </script>
 
     <script src='/js/admin.js'></script>
 @endpush
